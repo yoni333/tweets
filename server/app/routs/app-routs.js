e414a9a6255a29f1a668c7a58c638428acb8b70d
Yehonatan Yehezkel <yoni333@gmail.com>
2020-03-08 00:29:34 +0200

    add react static to heroku

--- server/app/routs/app-routs.js
+++ server/app/routs/app-routs.js
@@ -4,7 +4,9 @@ module.exports = function(app) {
   var appController = require('../controller/app-controller');
   var bulkDataController = require('../controller/bulk-data.controller');
     console.log('routs init')
-  app.route('/').get(appController.getAllTweets);
+  app.route('/').get(function (req, res) {
+    res.sendFile(path.join(__dirname, 'public/static', 'index.html'));
+  });
   app.route('/data').get(appController.getAllTweets);
   app.route('/more-then').get(appController.getTweetsMoreThan);
   app.route('/test-live').get(appController.getTestLiveServer);
--- server/app/server.js
+++ server/app/server.js
@@ -1,17 +1,22 @@
 const   express = require('express');
 const  bodyParser = require('body-parser');
+const favicon = require('express-favicon');
+const path = require('path');
 
 app = express(),
 
 port = process.env.PORT || 3000;
-app.listen(port);
 
+app.use(favicon(__dirname + '/public/static/favicon.ico'));
+// the __dirname is the current directory from where the script is running
+app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, 'public/static')));
 console.log('Jgive  RESTful API server started on: ' + port);
-var allowCrossDomain = function(req, res, next) {
+const  allowCrossDomain = function(req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
-
+  
   // intercept OPTIONS method
   if ('OPTIONS' == req.method) {
     res.send(200);
@@ -27,3 +32,5 @@ app.use(bodyParser.json());
 const  routes = require('./routs/app-routs');
 routes(app);
 console.log();
+
+app.listen(port);
