15d1cc26d05e7e16813bcf042f190055a6c00cbe
Yehonatan Yehezkel <yoni333@gmail.com>
2020-03-07 23:13:31 +0200

    allowCrossDomain in heroku

--- server/app/server.js
+++ server/app/server.js
@@ -7,8 +7,20 @@ port = process.env.PORT || 3000;
 app.listen(port);
 
 console.log('Jgive  RESTful API server started on: ' + port);
+var allowCrossDomain = function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
 
-
+  // intercept OPTIONS method
+  if ('OPTIONS' == req.method) {
+    res.send(200);
+  }
+  else {
+    next();
+  }
+};
+app.use(allowCrossDomain);
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
