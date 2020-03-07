8110d250d397771ff21cb4ea581a156048a5565d
Yehonatan Yehezkel <yoni333@gmail.com>
2020-03-07 22:31:44 +0200

    add home page

--- server/app/routs/app-routs.js
+++ server/app/routs/app-routs.js
@@ -4,6 +4,7 @@ module.exports = function(app) {
   var appController = require('../controller/app-controller');
   var bulkDataController = require('../controller/bulk-data.controller');
     console.log('routs init')
+  app.route('/').get(appController.getAllTweets);
   app.route('/data').get(appController.getAllTweets);
   app.route('/more-then').get(appController.getTweetsMoreThan);
   app.route('/test-live').get(appController.getTestLiveServer);
